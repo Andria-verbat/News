227f1c40d6d70265cb8e86fce7d639dad7862976
Andria <andria.verbat@gmail.com>
2023-01-01 15:16:51 -0500

    broadcast receiver done

--- app/src/main/java/com/assessment/news/activities/MainActivity.kt
+++ app/src/main/java/com/assessment/news/activities/MainActivity.kt
@@ -1,26 +1,40 @@
 package com.assessment.news.activities
 
+import android.content.IntentFilter
+import android.graphics.Color
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.assessment.news.R
 import com.assessment.news.activities.adapters.MainPageAdapter
 import com.assessment.news.databinding.ActivityMainBinding
+import com.assessment.news.utils.ConnectivityReceiver
 import com.assessment.news.viewModel.MainContentViewModel
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import java.util.Locale.filter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ConnectivityReceiver.ConnectivityReceiverListener
+   {
     private lateinit var binding : ActivityMainBinding
     private lateinit var viewModel: MainContentViewModel
     private lateinit var movieAdapter : MainPageAdapter
+    var languages = arrayOf("Java", "PHP", "Kotlin", "Javascript", "Python", "Swift")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        registerReceiver(ConnectivityReceiver(), IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
 
         prepareRecyclerView()
         viewModel = ViewModelProvider(this)[MainContentViewModel::class.java]
@@ -29,7 +43,9 @@ class MainActivity : AppCompatActivity() {
             movieAdapter.setNewsList(movieList)
         })
 
+
     }
+
     private fun prepareRecyclerView() {
         movieAdapter = MainPageAdapter()
         binding.recyclerView.apply {
@@ -37,5 +53,27 @@ class MainActivity : AppCompatActivity() {
             adapter = movieAdapter
         }
     }
+
+    override fun onNetworkConnectionChanged(isConnected: Boolean) {
+        showNetworkMessage(isConnected)
+    }
+
+    private fun showNetworkMessage(isConnected: Boolean) {
+        if (!isConnected) {
+
+            binding.toolbar.setBackgroundColor(resources.getColor(R.color.no_internet))
+            binding.txtNews.text="No Internet Connection Available"
+        } else {
+            binding.toolbar.setBackgroundColor(resources.getColor(R.color.toolbarColor))
+            binding.txtNews.text="News"
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ConnectivityReceiver.connectivityReceiverListener = this
+    }
+
+
 
 }
